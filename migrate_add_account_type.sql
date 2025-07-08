74f5565789209fbedeb76ebd4295d37694fca215
Mihir Sathvara <145009762+TraceX0@users.noreply.github.com>
2025-07-08 13:02:03 +0530

    Delete migrate_add_account_type.sql

--- migrate_add_account_type.sql
+++
@@ -1,4 +0,0 @@
--- Migration: Add account_type column to users table
-ALTER TABLE users ADD COLUMN account_type TEXT DEFAULT 'user';
--- Optionally, set admin for the admin user
-UPDATE users SET account_type = 'admin' WHERE username = 'admin';
